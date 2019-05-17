9770798fcb52778118639864cd385db1d843ed7e
Sitio <vinicius.c.e@hotmail.com>
2019-05-17 18:33:28 -0300

    Added preset queries

--- trabalho3/interface.py
+++ trabalho3/interface.py
@@ -27,33 +27,58 @@ def main():
             print("-----------------------------------------")
             for i in c:
                 print(i)
-            print("-----------------------------------------")
         # TODO: Create if-else for eache predefined operation
         # If not empty
         else:
             option = request[0]
             param  = request[2:]
 
+            # Get (cpf,name,email) from every person from {param} academic institution
             if option=='1':
-                result = c.execute(f"SELECT p.nome \
-            	                    FROM InstituicaoAcademica AS ia\
+                result = c.execute(f"SELECT p.cpf, p.nome, p.email \
+            	                    FROM InstituicaoAcademica AS ia \
                                     INNER JOIN Pessoa AS p \
-                                    ON ia.nome = '{param}' AND ia.cnpj = p.cnpj;"
+                                    ON ia.cnpj = p.cnpj
+                                    WHERE ia.nome = '{param}';"
                                     )
-            # elif option=='2':
-                
-            # elif option=='3':
-            
-            # elif option=='4':
-            
+            # Get (id) of every question from the subject with id=={param}
+            elif option=='2':
+                result = c.execute(f"SELECT id_questao \
+            	                    FROM Aborda \
+                                    WHERE id_materia = {param};"
+                                    )
+            # Lists the questions descriptions for every question from the test with id=={param}
+            elif option=='3':
+                result = c.execute(f"SELECT Questao.enunciado \
+            	                    FROM Questao INNER JOIN Compoe \
+                                    ON Questao.id = Compoe.id_questao \
+                                    WHERE c.id_prova = {param};"
+                                    )
+            # Lists (cpf) from all candidates whom took a test {param[0]} and is part of a certain institution {param[1]}
+            elif option=='4':
+                param = param.split(' ',1)
+                param[1] = param[1].strip()
+
+                result = c.execute(f"SELECT Pessoa.cpf \
+            	                    FROM Pessoa \
+                                    INNER JOIN InstituicaoAcademica AS ia \
+                                    ON ia.nome = {param[1]} AND ia.cnpj = Pessoa.cnpj \
+                                    INNER JOIN Prova AS p \
+                                    ON p.id = c.id_prova \
+                                    INNER JOIN Realiza AS r \
+                                    ON p.id = {param[0]} AND p.id = r.id_prova \
+                                    WHERE ia.id_prova = {param};"
+                                    )
+            # In case the number is no any of the options, labels as invalid
             else:
                 print("Opção inválida!")
                 print("=============================================")
                 continue
 
+            print("-----------------------------------------")
             for tup in result:
                 print(tup)
-                
+            print("-----------------------------------------")
 
         print("=============================================")
 
