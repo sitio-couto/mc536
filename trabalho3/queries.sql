3457072b65ace18a4e4fcd6f63a4ebf3b2b12e3f
Sitio <vinicius.c.e@hotmail.com>
2019-05-18 15:33:52 -0300

    Fixed SQL to new database

--- trabalho3/queries.sql
+++ trabalho3/queries.sql
@@ -3,24 +3,29 @@
 SELECT p.cpf, p.nome, p.email
 FROM InstituicaoAcademica AS ia INNER JOIN Pessoa AS p
 ON ia.cnpj = p.cnpj
-WHERE ia.nome = 'Faculdade Estadual da Bahia';
+WHERE ia.nome = 'Universidade Estadual do Mato Grosso';
 
 -- OP2 - Recupera o id de toda questao sobre Cálculo (id=1)
 SELECT id_questao
 FROM Aborda
 WHERE id_materia = 1;
 
--- OP3 - Lista enunciados das questões da Olimpíada Brasileira de Matemática Avançada (id=1)
+-- SELECT Questao.enunciado, Questao.id
+-- FROM Questao INNER JOIN Aborda
+-- ON Questao.id = Aborda.id_questao
+-- WHERE id_materia = 1;
+
+-- OP3 - Lista enunciados das questões da Olimpíada Brasileira de Matemática Avançada nivel 7 (id=1)
 SELECT Questao.enunciado
 FROM Questao INNER JOIN Compoe
 ON Questao.id = Compoe.id_questao
 WHERE Compoe.id_prova = 1;
 
--- OP4 - Lista cpf de todos que fizeram a Competição Brasileira de Informática Básica (id=16)
+-- OP4 - Lista cpf de todos que fizeram a Competição Brasileira de Informática nivel 10 (id=16)
 --       e sao integrantes da Faculdade Estadual da Bahia
 SELECT p.cpf
 FROM Pessoa AS p INNER JOIN InstituicaoAcademica AS ia
-ON ia.nome = 'Faculdade Estadual da Bahia' AND ia.cnpj = p.cnpj
+ON ia.nome = 'Universidade Estadual do Mato Grosso' AND ia.cnpj = p.cnpj
 INNER JOIN Realiza AS r
 ON r.id_prova = 16 AND p.cpf = r.cpf;
 
@@ -28,14 +33,15 @@ ON r.id_prova = 16 AND p.cpf = r.cpf;
 -- -- Apenas integrantes da Faculdade Estadual da Bahia
 -- SELECT p.nome
 -- FROM Pessoa AS p INNER JOIN InstituicaoAcademica AS ia
--- ON ia.nome = 'Faculdade Estadual da Bahia' AND ia.cnpj = p.cnpj;
--- -- Apenas pessoas que fizeram a Competição Brasileira de Informática Básica (id=16)
+-- ON ia.nome = 'Universidade Estadual do Mato Grosso' AND ia.cnpj = p.cnpj;
+-- -- Apenas pessoas que fizeram a Competição Brasileira de Informática nivel 10 (id=16)
 -- SELECT p.nome
 -- FROM Pessoa AS p INNER JOIN Realiza AS r
 -- ON r.id_prova = 16 AND p.cpf = r.cpf;
 
--- OP5 - Lista todas as provas de dificuldade Avançada
+-- OP5 - Lista todas as provas de nivel avançado (>=20)
 SELECT *
 FROM Prova
-WHERE Prova.nivel = 'Avançada';
+WHERE Prova.nivel >= 20;
 
+-- UPDATE "Prova" SET "nivel" = 3 WHERE "id" = 19
